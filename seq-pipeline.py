a4bebbfaf4899d5a2761f4b98e0a750913ef9218
Nate Johnson <jax523@gmail.com>
2023-02-16 11:31:54 -0300

    Update seq-pipeline.py
    Added ChIP functionality

--- seq-pipeline.py
+++ seq-pipeline.py
@@ -7,7 +7,39 @@ from pathlib import Path
 from os.path import isfile, isdir
 from pprint import pprint
 from time import sleep
+import sys
 
+
+###########################
+### Tools
+###########################
+
+
+class Logger(object):
+	def __init__(self, file_name):
+		self.terminal = sys.stdout
+		self.file_name = file_name
+
+		if isfile(file_name):
+			self.log = open(file_name, "a")
+		else:
+			self.log = open(file_name, "w")
+
+		# with open(file_name, "w") as outf:
+		# 	outf.write("")
+
+	def clear_ansi(self, message):
+		return(message.replace("\033[1m", "").replace("\033[0m",""))
+
+	def write(self, message):
+		self.terminal.write(message)
+		# with open(self.file_name, 'a') as outf:
+		# 	outf.write(message)  
+		self.log.write(self.clear_ansi(message))
+
+	def flush(self):
+		self.terminal.flush()
+		self.log.flush()
 
 
 def ispaired(srr_accession):
@@ -18,12 +50,19 @@ def ispaired(srr_accession):
 	else:
 		sys.exit("unknown if file is paired or single end!")
 
+
+
+###########################
+### Functions
+###########################
+
 def prefetch(srr_accession):
 	print()
 	print('prefetch...')
 
 	call = ['prefetch', srr_accession, '-v']
 	print("command:", " ".join(map(str,call)))
+	print()
 	p = Popen(call, stderr=PIPE, encoding='utf-8')
 
 	for line in p.stderr:
@@ -43,6 +82,7 @@ def fasterq_dump(srr_accession):
 
 	call = ['fasterq-dump', srr_accession, '-v', '-v', '-O', srr_accession]
 	print("command:", " ".join(map(str,call)))
+	print()
 	p = Popen(call, stderr=PIPE, encoding='utf-8')
 
 	for line in p.stderr:
@@ -66,6 +106,7 @@ def fastqc(srr_accession, suffix = ".fastq"):
 
 		call = ['fastqc', file, '--outdir', srr_accession]
 		print("command:", " ".join(map(str,call)))
+		print()
 		p = Popen(call, stderr=PIPE, stdout=PIPE, encoding='utf-8')
 
 		for line in p.stderr:
@@ -79,7 +120,7 @@ def fastqc(srr_accession, suffix = ".fastq"):
 
 
 
-def cutadapt(srr_accession):
+def cutadapt(srr_accession, cores):
 	print()
 	print('cutadapt...')
 
@@ -97,14 +138,13 @@ def cutadapt(srr_accession):
 	]
 
 
+	call = ['cutadapt', '-q', '25,25', '-m', '30', '-j', str(cores)] ## multicore doesn't work on my system....
 	if not ispaired(srr_accession):
-		call = ['cutadapt', '-q', '25,25', '-m', '30']
 		for adapter in adapters:
 			call += ['-b', adapter]
 		call += ['-o', f'{srr_accession}/{srr_accession}.trimmed.fastq', f'{srr_accession}/{srr_accession}.fastq']
 
 	else:
-		call = ['cutadapt', '-q', '25,25', '-m', '30']
 		for adapter in adapters:
 			call += ['-b', adapter]
 		for adapter in adapters:
@@ -115,6 +155,9 @@ def cutadapt(srr_accession):
 			f'{srr_accession}/{srr_accession}_1.fastq', 
 			f'{srr_accession}/{srr_accession}_2.fastq'
 		]
+
+	print("command:", " ".join(map(str,call)))
+	print()
 
 
 	p = Popen(call, stdout=PIPE, encoding='utf-8')
@@ -139,6 +182,7 @@ def kallisto_index(cdna_file):
 
 	call = ['kallisto', 'index','-i', index_file, cdna_file]
 	print("command:", " ".join(map(str,call)))
+	print()
 	p = Popen(call, stderr=PIPE, encoding='utf-8')
 
 	for line in p.stderr:
@@ -176,11 +220,216 @@ def kallisto_quant(srr_accession, cdna_file, bootstraps, length, sd):
 			f"{srr_accession}/{srr_accession}.trimmed.fastq"
 		]
 	print("command:"," ".join(map(str,call)))
+	print()
 	p = Popen(call, stderr=PIPE, encoding='utf-8')
 
 	for line in p.stderr:
 		print("[kallisto_quant]", line.strip())
 	p.wait()
+
+
+def bowtie2_build(genome_file):
+	print()
+	print('bowtie2-build...')
+
+	call = ['bowtie2-build', genome_file, genome_file]
+	print("command:", " ".join(map(str,call)))
+	print()
+	p = Popen(call, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+
+	for line in p.stdout:
+		print("[bowtie2-build]", line.strip())
+	for line in p.stderr:
+		print("[bowtie2-build]", line.strip())
+
+	p.wait()
+
+
+def bowtie2_align(srr_accession, genome_file, cores):
+	expected_index = genome_file + ".1.bt2"
+
+	if not isfile(expected_index):
+		print(f"Index not found ({expected_index}), performing bowtie2-build...")
+		bowtie2_build(genome_file)
+
+
+	print()
+	print('bowtie2-align...')
+
+	call = ["bowtie2", '-p', str(cores), '-x', genome_file]
+
+	if not ispaired(srr_accession):
+		call += ["-U", f"{srr_accession}/{srr_accession}.trimmed.fastq"]
+	else:
+		call += ["-1", f"{srr_accession}/{srr_accession}_1.trimmed.fastq"]
+		call += ["-2", f"{srr_accession}/{srr_accession}_2.trimmed.fastq"]
+
+
+	bam_file = f"{srr_accession}/{srr_accession}.bam"
+
+	if isfile(bam_file):
+		print(f'bam file ({bam_file}) found, skipping alignment')
+		return
+
+	print("command:", " ".join(map(str,call)), f"| samtools view -b -h > {bam_file}")
+	print()
+
+	with open(bam_file, 'wb') as outf:
+		p1 = Popen(call, stdout=PIPE, stderr=PIPE, encoding='utf-8')
+		p2 = Popen(['samtools','view','-b', '-h'], stdin=p1.stdout, stdout=outf)
+		p2.wait()
+
+
+
+
+
+
+
+def samtools_sort(srr_accession):
+
+	print()
+	print('samtools sort...')
+
+	bam_file = f"{srr_accession}/{srr_accession}.bam"
+	sorted_bam_file = f"{srr_accession}/{srr_accession}.sorted.bam"
+
+	if isfile(sorted_bam_file):
+		print(f'sorted bam file ({sorted_bam_file}) found, skipping sort')
+		return
+
+
+	call = ['samtools','sort',bam_file]
+	print("command:", " ".join(map(str,call)), f"> {sorted_bam_file}")
+	print()
+
+	with open(sorted_bam_file, 'wb') as outf:
+		p = Popen(call, stdout=outf, stderr=PIPE, encoding='utf-8')
+		for line in p.stderr:
+			print("[samtools_sort]", line.strip())
+		p.wait()
+
+
+def picard_deduplicate(srr_accession):
+
+	bam_file = f"{srr_accession}/{srr_accession}.sorted.bam"
+	dedup_file = f"{srr_accession}/{srr_accession}.dedup.bam"
+	metrics_file = f"{srr_accession}/{srr_accession}.dedup_metrics.txt"
+
+	print()
+	print('picard deduplicate...')
+
+
+	if isfile(dedup_file):
+		print(f'deduplicated bam file ({dedup_file}) found, skipping deduplication...')
+		return
+
+	call = ['picard', 'MarkDuplicates', '-I', bam_file, '-O', dedup_file, '-M', metrics_file]
+	print("command:", " ".join(map(str,call)))
+	print()
+
+	p = Popen(call, stderr=PIPE, stdout=PIPE, encoding='utf-8')
+
+	for line in p.stderr:
+		print("[picard_MarkDuplicates]", line.strip())
+	for line in p.stdout:
+		print("[picard_MarkDuplicates]", line.strip())
+	p.wait()
+
+def quality_filter(srr_accession):
+
+	print()
+	print('quality filter...')
+
+	dedup_file = f"{srr_accession}/{srr_accession}.dedup.bam"
+	filtered_file = f"{srr_accession}/{srr_accession}.filtered.bam"
+
+	if isfile(filtered_file):
+		print(f'quality filtered bam file ({filtered_file}) found, skipping filter...')
+		return
+
+	call = ['samtools', 'view', '-b', '-q', '10', dedup_file]
+	print("command:", " ".join(map(str,call)))
+	print()
+
+
+	with open(filtered_file, 'wb') as outf:
+		p = Popen(call, stdout=outf)
+		p.wait()
+
+def get_genome_size(output_directory, genome_file):
+
+
+	print()
+	print('measuring genome size...')
+
+	genome_size_file = f"{output_directory}/genome_size.txt"
+
+	if isfile(genome_size_file):
+		print(f'genome size file ({genome_size_file}) found, skipping measurement...')
+		with open(genome_size_file, 'r') as f:
+			return(f.readline().strip().split()[0])
+
+
+	call = ['wc', '-c', genome_file]
+	print("command:", " ".join(map(str,call)))
+	print()
+
+	with open(genome_size_file, 'w') as outf:
+
+		p = Popen(call, stdout=PIPE, encoding='utf-8')
+
+		for line in p.stdout:
+			print("[measure_genome]", line.strip())
+			print(line.strip(), file=outf)
+
+
+def macs2(treatments, controls, genome_file, genome_size, output_directory):
+
+	print()
+	print('getting peaks with macs2...')
+	call = ["macs2", 'callpeak']
+
+	call.append('-t')
+	for t in treatments:
+		call.append(f"{t}/{t}.dedup.bam")
+
+	if len(controls) > 0:
+		call.append('-c')
+		for c in controls:
+			call.append(f"{c}/{c}.dedup.bam")
+
+
+	if ispaired(treatments[0]):
+		call += ['-f', 'BAMPE']
+
+	call += ['-g', genome_size]
+	call += ['-n', 'macs_analysis']
+	call += ['--outdir', output_directory]
+
+	print("command:", " ".join(map(str,call)))
+	print()
+
+	p = Popen(call, stderr=PIPE, encoding='utf-8')
+	for line in p.stderr:
+			print("[macs2_callpeak]", line.strip())
+
+
+
+
+
+
+	# 	  "+file+" -c ./5-qual/qFiltered_SID01.SRR16905154_dedup.bam -n "+id2+".input --outdir 6-macs"]
+
+	# command = "macs2 callpeak -t "+file+" -c ./5-qual/qFiltered_SID01.SRR16905154_dedup.bam -f BAMPE -n "+id2+".input.bampe --outdir 6-macs"
+
+	# command = "macs2 callpeak -t "+file+" -f BAMPE -n "+id2+".bampe --outdir 6-macs"
+
+
+###########################
+### CLI commands
+###########################
+
+
 
 
 @click.group()
@@ -197,6 +446,16 @@ def cli():
 def download(srr_accession):
 	'''Download and QC of libraries.'''
 
+
+
+	Path(f"{srr_accession}").mkdir(parents=True, exist_ok=True)
+
+	log_file = f"{srr_accession}/{srr_accession}.log.txt"
+	with open(log_file, 'w') as outf:
+		outf.write("")
+	sys.stdout = Logger(log_file)
+
+
 	prefetch(srr_accession)
 	fasterq_dump(srr_accession)
 	fastqc(srr_accession)
@@ -210,10 +469,18 @@ def download(srr_accession):
 
 @cli.command()
 @click.argument("srr_accession")
+@click.option('-c','--cores',
+	default=1,
+	help='Number of cores used for cutadapt.')
 
-def trim(srr_accession):
+def trim(srr_accession, cores):
 	'''Trimming and QC of libraries.'''
-	cutadapt(srr_accession)
+
+
+	log_file = f"{srr_accession}/{srr_accession}.log.txt"
+	sys.stdout = Logger(log_file)
+
+	cutadapt(srr_accession, cores=cores)
 	fastqc(srr_accession, ".trimmed.fastq")
 
 
@@ -240,6 +507,10 @@ def trim(srr_accession):
 
 def kallisto(srr_accession, cdna_file, bootstraps, length, sd):
 	'''Quantification of RNAseq reads using kallisto.'''
+
+	log_file = f"{srr_accession}/{srr_accession}.log.txt"
+	sys.stdout = Logger(log_file)
+
 	kallisto_index(cdna_file)
 	kallisto_quant(srr_accession, cdna_file, bootstraps, length, sd)
 
@@ -251,15 +522,48 @@ def kallisto(srr_accession, cdna_file, bootstraps, length, sd):
 
 
 @cli.command()
-@click.argument("srr_accession")
-@click.argument("cdna_file")
+@click.argument("genome_file")
+@click.option('--cores',
+	default=1,
+	help='Number of cores used for bowtie2-align.')
 
-def chip(srr_accession, genome_file):
+@click.option('-o','--output_directory',
+	required=True,
+	help='Location to save chip experiment data')
+
+@click.option('-t','--treatments',
+	required=True,
+	multiple=True,
+	help='Treatment library SRR accessions, add multiple by using multiple flags')
+
+@click.option('-c','--controls',
+	required=False,
+	multiple=True,
+	help='Control library SRR accessions, add multiple by using multiple flags')
+
+def chip(genome_file, cores, treatments, controls, output_directory):
 	'''Analysis of chip data using macs2'''
 
-	print('not yet implemented')
+	for srr_accession in treatments + controls:
+
+		print()
+		print(f"PROCESSING {srr_accession}")
+
+		log_file = f"{srr_accession}/{srr_accession}.log.txt"
+		sys.stdout = Logger(log_file)
 
 
+		bowtie2_align(srr_accession, genome_file, cores)
+		samtools_sort(srr_accession)
+		picard_deduplicate(srr_accession)
+		quality_filter(srr_accession)
+
+	Path(f"{output_directory}").mkdir(parents=True, exist_ok=True)
+
+	genome_size = get_genome_size(output_directory, genome_file)
+
+
+	macs2(treatments, controls, genome_file, genome_size, output_directory)
 
 
 
@@ -271,12 +575,18 @@ def chip(srr_accession, genome_file):
 def cleanup(srr_accession):
 	'''Cleaning up heavy files'''
 
+	log_file = f"{srr_accession}/{srr_accession}.log.txt"
+	sys.stdout = Logger(log_file)
 
 	files = [
 		f'{srr_accession}/{srr_accession}.fastq',
 		f'{srr_accession}/{srr_accession}_1.fastq',
 		f'{srr_accession}/{srr_accession}_2.fastq',
-		f'{srr_accession}/{srr_accession}.sra'
+		f'{srr_accession}/{srr_accession}.sra',
+		f'{srr_accession}/{srr_accession}.bam',
+		f'{srr_accession}/{srr_accession}.sorted.bam',
+		f'{srr_accession}/{srr_accession}.filtered.bam',
+		f'{srr_accession}/{srr_accession}.dedup.bam'
 	]
 
 	print(files)
